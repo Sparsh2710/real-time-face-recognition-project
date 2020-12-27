41e8752b63a6c1706896a527c40eac52944a30e3
Sparsh2710 <70764422+Sparsh2710@users.noreply.github.com>
2020-12-27 12:35:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,25 @@ function setup() {
     Canvas.position(580, 370);
     Video = createCapture(VIDEO);
     Video.hide();
+
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZPjZV9yXP/model.json',modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
 }
 
 function draw() {
-    image(Video, 0, 0, 200, 200);   
+    image(Video, 0, 0, 200, 200); 
+    classifier.classify(Video, gotResult);  
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
